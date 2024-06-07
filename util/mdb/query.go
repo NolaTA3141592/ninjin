96399bf78c0d08f424ae40e68b3c5068940d755c
ReiAshizawa <rei11a2004@gmail.com>
2024-06-07 20:14:28 +0900

    add Query

---
+++ util/mdb/query.go
@@ -0,0 +1,24 @@
+package mdb
+
+import (
+	_ "github.com/lib/pq"
+)
+
+func (db *Mdb) Insert(SlackMessageID string, DiscordMessageID string) (error) {
+	sqlStatement := `
+    INSERT INTO MessageDataBase (slackID, discordID)
+    VALUES ($1, $2)`	
+	_, err := db.Data.Exec(sqlStatement, SlackMessageID, DiscordMessageID)
+	return err
+}
+
+func (db *Mdb) Query(slackID string) (string, error) {
+    var discordID string
+    sqlStatement := `SELECT name FROM users WHERE id=$1`
+    row := db.Data.QueryRow(sqlStatement, slackID)
+    err := row.Scan(&discordID)
+    if err != nil {
+        return "", err
+    }
+    return discordID, nil
+}
