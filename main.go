951e94516ff4f4e1bb1ff3db958392e6ceb501d6
Kousei-K <kousei123580@gmail.com>
2024-06-20 22:42:04 +0900

    複数ファイルの送信に対応

--- main.go
+++ main.go
@@ -84,9 +84,6 @@ func main() {
 			case "url_verification":
 				slack.Verify(w, r, body, SLACK_VERIFY_TOKEN)
 			case "event_callback":
-
-				fmt.Println(jsonbody)
-				
 				jsonbody2, ok := jsonbody["event"].(map[string]interface{})
 				if !ok {
 					http.Error(w, "Failed to parse json", http.StatusBadRequest)
