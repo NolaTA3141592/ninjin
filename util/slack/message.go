0db2e7cbf334c393cc9ff1a627ff3885a07730b2
Kousei-K <kousei123580@gmail.com>
2024-06-22 03:00:12 +0900

    レビューに対応

--- util/slack/message.go
+++ util/slack/message.go
@@ -19,62 +19,9 @@ func (sl SlackUtil) AttachMessageInfo(msg *cls.Message, data map[string]interfac
 	msg.Content = data["text"].(string)
 	msg.Slack_ID = data["ts"].(string)
 	msg.ChannelName = sl.GetChannelNameByID(data["channel"].(string))
-	if files, ok := data["files"].([]interface{}); ok {
-		for _, file := range files {
-			if fileMap, ok := file.(map[string]interface{}); ok {
-				if thumbs, ok := fileMap["thumb_360"]; ok {
-					msg.FileURLs = append(msg.FileURLs, thumbs.(string))
-				}
-				if other, ok := fileMap["url_private"]; ok {
-					msg.FileURLs = append(msg.FileURLs, other.(string))
-				}
-			}
-		}
-	}
-	if files, ok := data["files"].([]interface{}); ok {
-		for _, file := range files {
-			if fileMap, ok := file.(map[string]interface{}); ok {
-				if name, ok := fileMap["name"]; ok {
-					msg.FileNames = append(msg.FileNames, name.(string))
-				}
-			}
-		}
-	}
-
-	re := regexp.MustCompile(`<@[A-Z0-9]{11}>`)
-
-	matches := re.FindAllString(msg.Content, -1)
-
-	mentions := Mentions{
-		UserID_row:   []string{},
-		UserID_list:   []string{},
-		UserName: []string{},
-	}
-
-	mentions.UserID_row = append(mentions.UserID_row, matches...)
-
-	re = regexp.MustCompile(`<@(.*?)>`)
-
-	for _, match := range mentions.UserID_row {
-		userID := re.FindStringSubmatch(match)[1]
-		mentions.UserID_list = append(mentions.UserID_list, userID)
-	}
-
-	for _, userID := range mentions.UserID_list {
-		api := slackgo2.New(sl.SLACK_API_TOKEN)
-		userinfo, err := api.GetUserInfo(userID)
-		if err != nil {
-			return err
-		}
-		usernames := GetUserName(userinfo)
-		mentions.UserName = append(mentions.UserName, usernames)
-	}
-
-	for i := 0; i < len(mentions.UserID_list); i++ {
-		re := regexp.MustCompile(fmt.Sprintf("@%s", mentions.UserID_list[i]))
-		replacement := fmt.Sprintf("@%s", mentions.UserName[i])
-		msg.Content = re.ReplaceAllString(msg.Content, replacement)
-	}
+	msg.FileURLs = sl.GetFileURLs(msg, data)
+	msg.FileNames = sl.GetFileNames(msg, data)
+	msg.Content = sl.ReplaceMentions(msg)
 
 	return nil
 }
@@ -87,4 +34,71 @@ func (sl SlackUtil) GetChannelNameByID(channelID string) string {
 		return ""
 	}
 	return channelinfo.Name
+}
+
+func (sl SlackUtil) GetFileURLs(msg *cls.Message, data map[string]interface{}) []string {
+	FileURLs := []string{}
+	if files, ok := data["files"].([]interface{}); ok {
+		for _, file := range files {
+			if fileMap, ok := file.(map[string]interface{}); ok {
+				if thumbs, ok := fileMap["thumb_360"]; ok {
+					FileURLs = append(FileURLs, thumbs.(string))
+				}
+				if other, ok := fileMap["url_private"]; ok {
+					FileURLs = append(FileURLs, other.(string))
+				}
+			}
+		}
+	}
+	return FileURLs
+}
+
+func (sl SlackUtil) GetFileNames(msg *cls.Message, data map[string]interface{}) []string {
+	FileNames := []string{}
+	if files, ok := data["files"].([]interface{}); ok {
+		for _, file := range files {
+			if fileMap, ok := file.(map[string]interface{}); ok {
+				if name, ok := fileMap["name"]; ok {
+					FileNames = append(FileNames, name.(string))
+				}
+			}
+		}
+	}
+	return FileNames
+}
+
+func (sl SlackUtil) ReplaceMentions(msg *cls.Message) string {
+	message := msg.Content
+	re := regexp.MustCompile(`<@[A-Z0-9]{11}>`)
+	matches := re.FindAllString(message, -1)
+
+	mentions := Mentions{
+		UserID_row:   	[]string{},
+		UserID_list:   	[]string{},
+		UserName:		[]string{},
+	}
+
+	mentions.UserID_row = append(mentions.UserID_row, matches...)
+	re = regexp.MustCompile(`<@(.*?)>`)
+
+	for _, match := range mentions.UserID_row {
+		userID := re.FindStringSubmatch(match)[1]
+		mentions.UserID_list = append(mentions.UserID_list, userID)
+	}
+	for _, userID := range mentions.UserID_list {
+		api := slackgo2.New(sl.SLACK_API_TOKEN)
+		userinfo, err := api.GetUserInfo(userID)
+		if err != nil {
+			return ""
+		}
+		usernames := GetUserName(userinfo)
+		mentions.UserName = append(mentions.UserName, usernames)
+	}
+
+	for i := 0; i < len(mentions.UserID_list); i++ {
+		re := regexp.MustCompile(fmt.Sprintf("@%s", mentions.UserID_list[i]))
+		replacement := fmt.Sprintf("@%s", mentions.UserName[i])
+		message = re.ReplaceAllString(message, replacement)
+	}
+	return message
 }
